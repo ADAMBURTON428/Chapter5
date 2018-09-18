7c239de6e632c6093a4a8777ea748f7058408981
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-18 09:33:50 -0500

    update

--- Chapter5/src/practice/DoorGame.java
+++ Chapter5/src/practice/DoorGame.java
@@ -1,10 +1,43 @@
 package practice;
 
-public class DoorGame {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class DoorGame 
+{
 
+	public static void main(String[] args) 
+	{
+	
+		int doorChoice;
+		String result;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the amazing door choice game!");
+		System.out.println("Your host Steve Harvey");
+		System.out.println("Pick a door any door. \n1- for door One\n2- for door Two\n3- for door Three");
+		doorChoice = input.nextInt();
+		
+		if(doorChoice == 1) 
+		{
+			result = "a Pile of Gold";
+		}
+		else if(doorChoice == 2) 
+		{
+			result = "a Donkey have fun with that.";
+		}
+		else if(doorChoice == 3) 
+		{
+			result = "a million dollars in monopoly money";
+		}
+		else
+			result = "nothing because that was not a choice";
+		displayMessage(result);
+	}
+		
+
+		public static void displayMessage(String result) 
+		{
+		System.out.println("You won " + result);
+			
 	}
 
 }
