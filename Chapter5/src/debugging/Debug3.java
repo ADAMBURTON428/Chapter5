5e29f9b487591bcb49a67ab5ba4a1f74735c26d4
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-25 09:15:48 -0500

    update

--- Chapter5/src/debugging/Debug3.java
+++ Chapter5/src/debugging/Debug3.java
@@ -1,9 +1,35 @@
 package debugging;
 
-public class Debug3 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Debug3 
+{
+
+	public static void main(String[] args) 
+	{
+		   int item = 0;
+		      String output = null;
+		      final int LOW = 111;
+		      final int HIGH = 9;
+		      final int CUTOFF = 500;
+		      Scanner input = new Scanner(System.in);
+		      calculateOutPut(item, output, LOW, HIGH, CUTOFF);
+		   }
+		   public static void calculateOutPut(int item, String output, final int LOW, final int HIGH, final int CUTOFF)
+		   {
+			   Scanner input = new Scanner(System.in); 
+		      System.out.println("Please enter item number");
+		      item = input.nextInt();
+		      if(item < LOW) output = "Item number too low";
+		      else
+		if(item > HIGH)
+		output = "Item number too high";
+		      else
+		if(item > CUTOFF)
+		             output = "Valid - in Automotive Department";
+		          else
+		             output = "Valid - Item in Housewares Department";
+		       System.out.println(output);
 
 	}
 
