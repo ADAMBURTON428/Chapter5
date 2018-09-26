44d462b2b188325c238a4369478f6d79bb194c28
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-26 09:49:41 -0500

    why

--- Chapter5/src/excercises/Pay.java
+++ Chapter5/src/excercises/Pay.java
@@ -11,17 +11,17 @@ public class Pay
 		String Skill;
 		int Insurance;
 		double hourlyPay;
-		double Pay;
+		double FinalPay;
 		double overTime;
 		double netPay;
 		Scanner input = new Scanner(System.in);
 		
 		
-		System.out.print("Enter Your Skill Level(1-3)");
+		System.out.print("Enter Your Skill Level(1-3): ");
 		Skill = input.nextLine();
-		System.out.print("Enter your Hours worked");
+		System.out.print("Enter your Hours worked: ");
 		Hours = input.nextInt();
-		System.out.print("Enter your Insurance \n1-Medical \n2-Dental \n3-Long term disability ");
+		System.out.print("Enter your Insurance \n1-Medical \n2-Dental \n3-Long term disability: ");
 		Insurance = input.nextInt();
 		
 		switch(Skill) 
@@ -31,9 +31,10 @@ public class Pay
 			{
 			hourlyPay = Hours * 17;
 			overTime = (Hours - 40) * 17 * 1.5;
-			
+			FinalPay = hourlyPay + overTime;
 			System.out.println("Your pay is "+ hourlyPay);
 			System.out.println("Your overtime pay is " + overTime );
+			System.out.println("Your Pay is " + FinalPay );
 				
 			}
 			else
@@ -43,17 +44,50 @@ public class Pay
 			
 		case "2":
 			
+			if(Hours>40 && Insurance == 1) 
+			{
+			hourlyPay = Hours * 17;
+			overTime = (Hours - 40) * 17 * 1.5;
+			FinalPay = hourlyPay + overTime;
+			netPay = FinalPay - 32.50;
+					
+			System.out.println("Your pay is "+ hourlyPay);
+			System.out.println("Your overtime pay is " + overTime );
+			System.out.println("Your Pay is " + FinalPay );
+			System.out.println("Your net pay is " + netPay );
+			
+				
+			}
+			else if(Hours>40 && Insurance == 2) 
+			{
+			hourlyPay = Hours * 17;
+			overTime = (Hours - 40) * 17 * 1.5;
+			FinalPay = hourlyPay + overTime;
+			netPay = FinalPay - 20.00;
+					
+			System.out.println("Your pay is "+ hourlyPay);
+			System.out.println("Your overtime pay is " + overTime );
+			System.out.println("Your Pay is " + FinalPay );
+			System.out.println("Your net pay is " + netPay );
+			
 			
 		
-		
-		
-		
-		
-		}
 
-	
 		
-
+			}
+			else if(Hours>40 && Insurance == 3) 
+			{
+			hourlyPay = Hours * 17;
+			overTime = (Hours - 40) * 17 * 1.5;
+			FinalPay = hourlyPay + overTime;
+			netPay = FinalPay - 10.00;
+					
+			System.out.println("Your pay is "+ hourlyPay);
+			System.out.println("Your overtime pay is " + overTime );
+			System.out.println("Your Pay is " + FinalPay );
+			System.out.println("Your net pay is " + netPay );
+				
+			}
 	}
-
+	}
 }
