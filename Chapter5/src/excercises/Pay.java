3dc318367242a066c211dda7e91faaf0c0a44db2
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-26 09:49:01 -0500

    ??? update

--- Chapter5/src/excercises/Pay.java
+++ Chapter5/src/excercises/Pay.java
@@ -1,17 +1,57 @@
 package excercises;
 
+import java.util.Scanner;
+
 public class Pay 
 {
 
 	public static void main(String[] args) 
 	{
 		int Hours;
-		int Skill;
+		String Skill;
 		int Insurance;
 		double hourlyPay;
 		double Pay;
 		double overTime;
 		double netPay;
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Enter Your Skill Level(1-3)");
+		Skill = input.nextLine();
+		System.out.print("Enter your Hours worked");
+		Hours = input.nextInt();
+		System.out.print("Enter your Insurance \n1-Medical \n2-Dental \n3-Long term disability ");
+		Insurance = input.nextInt();
+		
+		switch(Skill) 
+		{
+		case "1":
+			if(Hours>40) 
+			{
+			hourlyPay = Hours * 17;
+			overTime = (Hours - 40) * 17 * 1.5;
+			
+			System.out.println("Your pay is "+ hourlyPay);
+			System.out.println("Your overtime pay is " + overTime );
+				
+			}
+			else
+				hourlyPay = Hours * 17;
+				System.out.print("Your pay is " + hourlyPay);
+				break;
+			
+		case "2":
+			
+			
+		
+		
+		
+		
+		
+		}
+
+	
 		
 
 	}
